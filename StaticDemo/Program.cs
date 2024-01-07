7f6c23140ff24918796f3bbdb95ba150d06a1a42
shivindewar <indewars@gmail.com>
2024-01-07 19:41:21 +0530

    static class demo

---
+++ StaticDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius = 37; double fahernheit = 98.6;
+
+            Console.WriteLine("Value of {0} celcius to fahrenheit is {1}",
+               celcius, convertor.ToFahrenheit(celcius));
+
+            Console.WriteLine("Value of {0} fahrenheit to celcius is {1}",
+               fahernheit, convertor.ToCelcius(fahernheit));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StaticDemo/convertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    public static class convertor
+    {
+        public static double ToFahrenheit(double celcius)
+        {
+
+            return (celcius * 9/5) + 32;
+        }
+
+        public static double ToCelcius(double fahernheit)
+        { 
+            return (fahernheit - 32) * 5/9;
+        }
+    }
+}
